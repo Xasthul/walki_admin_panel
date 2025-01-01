1de4d485d083a996c73986ff5b5f704fd70845d1
nazarii <nazarii.honcharenko@haven.com>
2025-01-01 22:16:54 +0200

    Add empty state for users_table.dart

--- lib/home/users/utils/widget/users_table.dart
+++ lib/home/users/utils/widget/users_table.dart
@@ -15,45 +15,53 @@ class _UsersTableState extends State<UsersTable> {
 
   @override
   Widget build(BuildContext context) => Observer(
-        builder: (context) => DataTable(
-          border: TableBorder.all(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors.grey,
-          ),
-          sortColumnIndex: _store.sortColumnIndex,
-          sortAscending: _store.sortAscending,
-          columns: [
-            DataColumn(
-              label: const Text('Email'),
-              onSort: _store.onEmailSort,
-            ),
-            DataColumn(
-              label: const Text('Name'),
-              onSort: _store.onNameSort,
-            ),
-            DataColumn(
-              label: const Text('Places visited'),
-              numeric: true,
-              onSort: _store.onPlacesVisitedSort,
-            ),
-            DataColumn(
-              label: const Text('Reviews written'),
-              numeric: true,
-              onSort: _store.onReviewsWrittenSort,
-            ),
-          ],
-          rows: _store.users
-              .map(
-                (user) => DataRow(
-                  cells: [
-                    DataCell(Text(user.email)),
-                    DataCell(Text(user.name)),
-                    DataCell(Text('${user.placesVisited}')),
-                    DataCell(Text('${user.reviewsWritten}')),
-                  ],
+        builder: (context) {
+          if (_store.users.isNotEmpty) {
+            return DataTable(
+              border: TableBorder.all(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.grey,
+              ),
+              sortColumnIndex: _store.sortColumnIndex,
+              sortAscending: _store.sortAscending,
+              columns: [
+                DataColumn(
+                  label: const Text('Email'),
+                  onSort: _store.onEmailSort,
                 ),
-              )
-              .toList(),
-        ),
+                DataColumn(
+                  label: const Text('Name'),
+                  onSort: _store.onNameSort,
+                ),
+                DataColumn(
+                  label: const Text('Places visited'),
+                  numeric: true,
+                  onSort: _store.onPlacesVisitedSort,
+                ),
+                DataColumn(
+                  label: const Text('Reviews written'),
+                  numeric: true,
+                  onSort: _store.onReviewsWrittenSort,
+                ),
+              ],
+              rows: _store.users
+                  .map(
+                    (user) => DataRow(
+                      cells: [
+                        DataCell(Text(user.email)),
+                        DataCell(Text(user.name)),
+                        DataCell(Text('${user.placesVisited}')),
+                        DataCell(Text('${user.reviewsWritten}')),
+                      ],
+                    ),
+                  )
+                  .toList(),
+            );
+          }
+          return const Text(
+            'There are no users yet.',
+            style: TextStyle(fontSize: 16),
+          );
+        },
       );
 }
