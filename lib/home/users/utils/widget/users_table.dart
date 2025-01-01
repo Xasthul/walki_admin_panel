959f176018c52afe69594bcd4b6883b0a2fefeb2
nazarii <nazarii.honcharenko@haven.com>
2025-01-01 23:42:03 +0200

    Add total label for users table

--- lib/home/users/utils/widget/users_table.dart
+++ lib/home/users/utils/widget/users_table.dart
@@ -17,45 +17,55 @@ class _UsersTableState extends State<UsersTable> {
   Widget build(BuildContext context) => Observer(
         builder: (context) {
           if (_store.users.isNotEmpty) {
-            return DataTable(
-              border: TableBorder.all(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.grey,
-              ),
-              sortColumnIndex: _store.sortColumnIndex,
-              sortAscending: _store.sortAscending,
-              columns: [
-                DataColumn(
-                  label: const Text('Email'),
-                  onSort: _store.onEmailSort,
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Total: ${_store.users.length}',
+                  style: const TextStyle(fontSize: 16),
                 ),
-                DataColumn(
-                  label: const Text('Name'),
-                  onSort: _store.onNameSort,
-                ),
-                DataColumn(
-                  label: const Text('Places visited'),
-                  numeric: true,
-                  onSort: _store.onPlacesVisitedSort,
-                ),
-                DataColumn(
-                  label: const Text('Reviews written'),
-                  numeric: true,
-                  onSort: _store.onReviewsWrittenSort,
+                const SizedBox(height: 12),
+                DataTable(
+                  border: TableBorder.all(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey,
+                  ),
+                  sortColumnIndex: _store.sortColumnIndex,
+                  sortAscending: _store.sortAscending,
+                  columns: [
+                    DataColumn(
+                      label: const Text('Email'),
+                      onSort: _store.onEmailSort,
+                    ),
+                    DataColumn(
+                      label: const Text('Name'),
+                      onSort: _store.onNameSort,
+                    ),
+                    DataColumn(
+                      label: const Text('Places visited'),
+                      numeric: true,
+                      onSort: _store.onPlacesVisitedSort,
+                    ),
+                    DataColumn(
+                      label: const Text('Reviews written'),
+                      numeric: true,
+                      onSort: _store.onReviewsWrittenSort,
+                    ),
+                  ],
+                  rows: _store.users
+                      .map(
+                        (user) => DataRow(
+                          cells: [
+                            DataCell(Text(user.email)),
+                            DataCell(Text(user.name)),
+                            DataCell(Text('${user.placesVisited}')),
+                            DataCell(Text('${user.reviewsWritten}')),
+                          ],
+                        ),
+                      )
+                      .toList(),
                 ),
               ],
-              rows: _store.users
-                  .map(
-                    (user) => DataRow(
-                      cells: [
-                        DataCell(Text(user.email)),
-                        DataCell(Text(user.name)),
-                        DataCell(Text('${user.placesVisited}')),
-                        DataCell(Text('${user.reviewsWritten}')),
-                      ],
-                    ),
-                  )
-                  .toList(),
             );
           }
           return const Text(
